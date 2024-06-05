19c7549b6e5319d3a22c850beae42bf7aa6a8139
Marlon Etheredge <marlon.etheredge@uibk.ac.at>
2024-06-05 14:53:36 +0200

    feat: generate response time data

--- railway/scripts/services/light.py
+++ railway/scripts/services/light.py
@@ -3,6 +3,29 @@ from pydantic import BaseModel
 from starlette.responses import Response
 from starlette.status import HTTP_200_OK
 
+from opentelemetry import metrics
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
+from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import OTLPMetricExporter
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+
+import os
+
+resource = Resource(attributes={SERVICE_NAME: "railway-simulation"})
+
+exporter = OTLPMetricExporter(endpoint=os.environ["OTLP_ENPOINT"])
+
+metric_reader = PeriodicExportingMetricReader(
+    exporter, export_interval_millis=int(os.environ["METRICS_INTERVAL"])
+)
+
+provider = MeterProvider(resource=resource, metric_readers=[metric_reader])
+
+metrics.set_meter_provider(provider)
+meter = metrics.get_meter("railway")
+
+light_status_counter = meter.create_gauge("gate_status")
+
 app = FastAPI()
 
 
@@ -20,11 +43,15 @@ def get_light_status():
 
 @app.post("/light/on")
 def light_on():
+    light_status_counter.set(1)
+
     light_status.status = "on"
     return Response(status_code=HTTP_200_OK)
 
 
 @app.post("/light/off")
 def light_off():
+    light_status_counter.set(0)
+
     light_status.status = "off"
     return Response(status_code=HTTP_200_OK)
