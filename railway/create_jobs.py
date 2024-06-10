8afa065031124c7c54c346fb81b929470367a3fe
Marlon Etheredge <marlon.etheredge@uibk.ac.at>
2024-06-10 15:59:00 +0200

    feat: adding job creation script

---
+++ railway/create_jobs.py
@@ -0,0 +1,43 @@
+from kazoo.client import KazooClient
+
+import argparse
+import os
+
+from pathlib import Path
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--host", type=str, default="127.0.0.1:2181", help="ZooKeeper host address"
+    )
+
+    args = parser.parse_args()
+
+    # Create the KazooClient using the provided host
+    zk = KazooClient(hosts=args.host)
+
+    try:
+        # Start the Kazoo Client
+        zk.start()
+
+        # Ensure the /jobs node exists, create if necessary
+        zk.ensure_path("/jobs")
+
+        job_dir = "railway/job"
+
+        for job in os.listdir(job_dir):
+            with open(os.path.join(job_dir, job)) as file:
+                job_json = file.read()
+
+            job_json_bytes: bytes = job_json.encode("utf-8")
+
+            job_name = Path(job).stem
+
+            job_path = f"/jobs/{job_name}"
+
+            zk.create(job_path, job_json_bytes)
+            print(f"Node '{job_path}' created")
+    finally:
+        # Stop the client
+        zk.stop()
