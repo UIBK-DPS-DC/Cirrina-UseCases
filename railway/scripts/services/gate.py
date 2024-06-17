c4c8a4fa0f09ec637bfae665246ea7dca134d7d6
Marlon Etheredge <marlon.etheredge@uibk.ac.at>
2024-06-17 14:53:28 +0200

    feat: simplifying services

--- railway/scripts/services/gate.py
+++ railway/scripts/services/gate.py
@@ -3,29 +3,7 @@ from pydantic import BaseModel
 from starlette.responses import Response
 from starlette.status import HTTP_200_OK
 
-from opentelemetry import metrics
-from opentelemetry.sdk.metrics import MeterProvider
-from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
-from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import OTLPMetricExporter
-from opentelemetry.sdk.resources import SERVICE_NAME, Resource
-
-import os
 import time
-
-resource = Resource(attributes={SERVICE_NAME: "railway-simulation"})
-
-exporter = OTLPMetricExporter(endpoint=os.environ["OTLP_ENDPOINT"])
-
-metric_reader = PeriodicExportingMetricReader(
-    exporter, export_interval_millis=int(os.environ["METRICS_INTERVAL"])
-)
-
-provider = MeterProvider(resource=resource, metric_readers=[metric_reader])
-
-metrics.set_meter_provider(provider)
-meter = metrics.get_meter("railway")
-
-gate_response_time_gauge = meter.create_gauge("gate_response_time")
 
 app = FastAPI()
 
@@ -46,15 +24,6 @@ def gate_down(request: Request):
     # Create the gate status if not seen before
     if id not in gate_statuses:
         gate_statuses[id] = GateStatus(status="up", last=time.time())
-
-    # Train is coming, capture response time
-    if gate_statuses[id].status == "up":
-        t = time.time()
-        dt = t - gate_statuses[id].last
-
-        gate_response_time_gauge.set(dt)
-
-        gate_statuses[id].last = t
 
     # Update its state
     gate_statuses[id].status = "down"
--- railway/scripts/services/light.py
+++ railway/scripts/services/light.py
@@ -3,29 +3,8 @@ from pydantic import BaseModel
 from starlette.responses import Response
 from starlette.status import HTTP_200_OK
 
-from opentelemetry import metrics
-from opentelemetry.sdk.metrics import MeterProvider
-from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
-from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import OTLPMetricExporter
-from opentelemetry.sdk.resources import SERVICE_NAME, Resource
-
 import os
 import time
-
-resource = Resource(attributes={SERVICE_NAME: "railway-simulation"})
-
-exporter = OTLPMetricExporter(endpoint=os.environ["OTLP_ENDPOINT"])
-
-metric_reader = PeriodicExportingMetricReader(
-    exporter, export_interval_millis=int(os.environ["METRICS_INTERVAL"])
-)
-
-provider = MeterProvider(resource=resource, metric_readers=[metric_reader])
-
-metrics.set_meter_provider(provider)
-meter = metrics.get_meter("railway")
-
-light_response_time_gauge = meter.create_gauge("light_response_time")
 
 app = FastAPI()
 
@@ -46,15 +25,6 @@ async def light_on(request: Request):
     # Create the gate status if not seen before
     if id not in light_statuses:
         light_statuses[id] = LightStatus(status="off", last=time.time())
-
-    # Train is coming, capture response time
-    if light_statuses[id].status == "off":
-        t = time.time()
-        dt = t - light_statuses[id].last
-
-        light_response_time_gauge.set(dt)
-
-        light_statuses[id].last = t
 
     # Update its state
     light_statuses[id].status = "on"
